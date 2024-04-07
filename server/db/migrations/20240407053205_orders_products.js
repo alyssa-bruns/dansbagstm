319b62773a6cfc438784becd784963f54c4ffa8f
Alyssa <alynicbru@gmail.com>
2024-04-07 17:33:22 +1200

    Create orders_products migration

---
+++ server/db/migrations/20240407053205_orders_products.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('orders_products', (table) => {
+    table.number('order_id')
+    table.number('product_id')
+  })
+}
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('orders_products')
+}
