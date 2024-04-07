035f03b21641d25bbc394456385dae391ac873ee
Alyssa <alynicbru@gmail.com>
2024-04-07 17:18:11 +1200

    Create add-ons table migration

---
+++ server/db/migrations/20240407051601_add_ons.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('add_ons', (table) => {
+    table.increments('id')
+    table.string('name')
+    table.number('price')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('add_ons')
+}
