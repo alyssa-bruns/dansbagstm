40cb53449685328c2302b9865948e744eeb6d08a
Alyssa <alynicbru@gmail.com>
2024-04-07 17:15:27 +1200

    Create products table migration

---
+++ server/db/migrations/20240407050549_products.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('products', (table) => {
+    table.increments('id')
+    table.string('name')
+    table.string('color_one')
+    table.string('color_two')
+    table.number('price')
+    table.number('add_on_id')
+    table.boolean('is_custom')
+    table.string('image')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+export function down(knex) {
+  return knex.schema.dropTable('products')
+}
