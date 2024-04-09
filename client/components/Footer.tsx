a2f96c69bfec9a131fa1cfce291e9c0dac6aca0f
Alyssa <alynicbru@gmail.com>
2024-04-09 16:40:22 +1200

    Start adding to Footer and Home

---
+++ client/components/Footer.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div>
+          <a>Contact</a>
+        </div>
+        <div>
+          <p>© Alyssa Bruns 2024</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
