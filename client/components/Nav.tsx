440949dec50fd4b8cec9657ff0fa757d646924ce
Alyssa <alynicbru@gmail.com>
2024-04-09 16:28:46 +1200

    Route setup

---
+++ client/components/Nav.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+
+function Nav() {
+  return (
+    <>
+      <nav>
+        <div>
+          <a>DANSBAGSTM</a>
+        </div>
+        <div>
+          <a>Products</a>
+          <a>FAQ</a>
+          <a>Gallery</a>
+          <a>Blog</a>
+          <a>Login</a>
+          <a>Cart</a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Nav
--- client/router.tsx
+++ client/router.tsx
@@ -7,7 +7,32 @@ import {
 import App from './components/App'
 
 const router = createBrowserRouter(
-  createRoutesFromElements([<Route path="/" element={<App />} />]),
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="products">
+          <Route index element={<Products />} />
+          <Route path=":id" element={<Product />} />
+        </Route>
+        {/* <Route path="gallery">
+          <Route index element={<Gallery />} />
+        </Route>
+        <Route path="faq">
+          <Route index element={<Faq />} />
+        </Route>
+        <Route path="blog">
+          <Route index element={<Blog />} />
+        </Route>
+        <Route path="cart">
+          <Route index element={<Cart />} />
+        </Route> */}
+        {/* <Route path="login">
+          <Route index element={<Login />} />
+        </Route> */}
+      </Route>
+    </>,
+  ),
 )
 
 export default router
