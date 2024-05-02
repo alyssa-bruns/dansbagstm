8df1197f92cccf0334904d3488fd0f8071c9ee9a
Alyssa <alynicbru@gmail.com>
2024-05-03 11:01:36 +1200

    Update options_products table with secondary color ids

--- server/db/seeds/options_products.js
+++ server/db/seeds/options_products.js
@@ -53,6 +53,30 @@ export async function seed(knex) {
       option_id: 8,
     },
     {
+      product_id: 2,
+      option_id: 10,
+    },
+    {
+      product_id: 2,
+      option_id: 11,
+    },
+    {
+      product_id: 2,
+      option_id: 12,
+    },
+    {
+      product_id: 2,
+      option_id: 13,
+    },
+    {
+      product_id: 2,
+      option_id: 14,
+    },
+    {
+      product_id: 2,
+      option_id: 15,
+    },
+    {
       product_id: 3,
       option_id: 3,
     },
@@ -75,6 +99,30 @@ export async function seed(knex) {
     {
       product_id: 3,
       option_id: 8,
+    },
+    {
+      product_id: 3,
+      option_id: 10,
+    },
+    {
+      product_id: 3,
+      option_id: 11,
+    },
+    {
+      product_id: 3,
+      option_id: 12,
+    },
+    {
+      product_id: 3,
+      option_id: 13,
+    },
+    {
+      product_id: 3,
+      option_id: 14,
+    },
+    {
+      product_id: 3,
+      option_id: 15,
     },
     {
       product_id: 4,
@@ -113,6 +161,30 @@ export async function seed(knex) {
       option_id: 9,
     },
     {
+      product_id: 4,
+      option_id: 10,
+    },
+    {
+      product_id: 4,
+      option_id: 11,
+    },
+    {
+      product_id: 4,
+      option_id: 12,
+    },
+    {
+      product_id: 4,
+      option_id: 13,
+    },
+    {
+      product_id: 4,
+      option_id: 14,
+    },
+    {
+      product_id: 4,
+      option_id: 15,
+    },
+    {
       product_id: 5,
       option_id: 1,
     },
@@ -149,6 +221,30 @@ export async function seed(knex) {
       option_id: 9,
     },
     {
+      product_id: 5,
+      option_id: 10,
+    },
+    {
+      product_id: 5,
+      option_id: 11,
+    },
+    {
+      product_id: 5,
+      option_id: 12,
+    },
+    {
+      product_id: 5,
+      option_id: 13,
+    },
+    {
+      product_id: 5,
+      option_id: 14,
+    },
+    {
+      product_id: 5,
+      option_id: 15,
+    },
+    {
       product_id: 6,
       option_id: 3,
     },
@@ -171,6 +267,30 @@ export async function seed(knex) {
     {
       product_id: 6,
       option_id: 8,
+    },
+    {
+      product_id: 6,
+      option_id: 10,
+    },
+    {
+      product_id: 6,
+      option_id: 11,
+    },
+    {
+      product_id: 6,
+      option_id: 12,
+    },
+    {
+      product_id: 6,
+      option_id: 13,
+    },
+    {
+      product_id: 6,
+      option_id: 14,
+    },
+    {
+      product_id: 6,
+      option_id: 15,
     },
   ])
 }
